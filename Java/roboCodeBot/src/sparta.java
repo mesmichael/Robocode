09bd143a45bbe92a8f472f9925536370fd90ae7d
mesmichael <likefunnycool@gmail.com>
2013-12-12 10:03:24 -0500

    MOOBOT

--- Java/roboCodeBot/src/sparta.java
+++ Java/roboCodeBot/src/sparta.java
@@ -5,6 +5,7 @@ import robocode.TeamRobot.*;
 import robocode.util.Utils;
 import java.awt.geom.*;
 import java.util.*;
+import java.util.List;
 import java.awt.*;
 
 
@@ -32,11 +33,20 @@ public class sparta extends TeamRobot{
 	 double absoluteBearing = 0;
 	 double enemyY = 0;
 	 double enemyX = 0;
-	 
-	public void EnemyWave() {}
+	 int[][] stats = new int[13][31];
+	 private double startX, startY, startBearing, power;
+	 private long   fireTime;
+	 private int    direction;
+	 private int[]  returnSegment;
+	 List<sparta> waves = new ArrayList<sparta>();
 	
 	 
-	 public void run() {
+	 public sparta(double x, double y, double absBearing, double power2,
+			int direction2, long time, int[] currentStats) {
+		// TODO Auto-generated constructor stub
+	}
+
+	public void run() {
 		 //all parts (gun, body, radar) move indepentent from one each other
 		 setAdjustRadarForRobotTurn(true);
 		 setAdjustGunForRobotTurn(true);
@@ -56,9 +66,52 @@ public class sparta extends TeamRobot{
 	
 	 }
 	 
+	 public void WaveBullet(double x, double y, double bearing, double power,
+				int direction, long time, int[] segment)
+		{
+			startX         = x;
+			startY         = y;
+			startBearing   = bearing;
+			this.power     = power;
+			this.direction = direction;
+			fireTime       = time;
+			returnSegment  = segment;
+		}
+	 	
+	 public double getBulletSpeed()
+		{
+			return 20 - power * 3;
+		}
 	 
-		
+	public double maxEscapeAngle()
+		{
+			return Math.asin(8 / getBulletSpeed());
+		}
+	 
+	public boolean checkHit(double enemyX, double enemyY, long currentTime)
+	{
+		// if the distance from the wave origin to our enemy has passed
+		// the distance the bullet would have traveled...
+		if (Point2D.distance(startX, startY, enemyX, enemyY) <= 
+				(currentTime - fireTime) * getBulletSpeed())
+		{
+			double desiredDirection = Math.atan2(enemyX - startX, enemyY - startY);
+			double angleOffset = Utils.normalRelativeAngle(desiredDirection - startBearing);
+			double guessFactor =
+				Math.max(-1, Math.min(1, angleOffset / maxEscapeAngle())) * direction;
+			int index = (int) Math.round((returnSegment.length - 1) /2 * (guessFactor + 1));
+			returnSegment[index]++;
+			return true;
+		}
+		return false;
+	}
 
+	
+	
+	
+	
+	
+	
 	private void move() {
 		// TODO Auto-generated method stub
 		ahead(Double.POSITIVE_INFINITY * moveDirection);
@@ -90,100 +143,63 @@ public class sparta extends TeamRobot{
 		 targetVelocity = e.getVelocity();
 		 distance = e.getDistance();
 		 bulletPower = bPower;
-		 double bullet = 20-3*bulletPower;
-		 double angel = (bullet * distance);
-		 double speedangel = targetVelocity * bullet;
+		 double angel = (getBulletSpeed() * distance);
+		 double speedangel = targetVelocity * getBulletSpeed();
 		 heading = getHeading();
 		 absoluteBearing = getHeadingRadians() + e.getBearingRadians();		 
 		 enemyX = getX() + e.getDistance() * Math.sin(absoluteBearing);
 		 enemyY = getY() + e.getDistance() * Math.cos(absoluteBearing);
 		 double enemyHeadingChange = targetHeading - oldTargetHeading;
 		 oldTargetHeading = targetHeading;
-		 double deltaTime = 0;
-		 double predictedX = enemyX, predictedY = enemyY;
-	
+		 double absBearing = getHeadingRadians() + e.getBearingRadians();
 		 
-		if (isTeammate(e.getName()) == false) {
-		 //turns so that alwas sidewase if enemy
-		setTurnRight(e.getBearing() + 90);
-			//calculates the power of the robots shot
-			 if(distance > 350)
-			 {
-				 bPower = 0.5; 
-			 }
-			 if(distance > 300)
-			 {
-				 bPower = 1; 
-			 }
-			 if(distance > 250)
-			 {
-				 bPower = 2; 
-			 }
-			 else
-			 {
-				 bPower = 3; 
-			 }
-			
-			 // creates a lock on a robot
-			 double radarTurn = getHeadingRadians() + e.getBearingRadians() - getRadarHeadingRadians();
-			 setTurnRadarRightRadians(Utils.normalRelativeAngle(radarTurn));
-			 
-			 
-			 //perdicts acording to power lvl
-			 double headOnBearing = getHeadingRadians() + e.getBearingRadians();
-			 double linearBearing = headOnBearing + Math.asin(e.getVelocity() / Rules.getBulletSpeed(bPower) * Math.sin(e.getHeadingRadians() - headOnBearing));
-			 setTurnGunRightRadians(Utils.normalRelativeAngle(linearBearing - getGunHeadingRadians()));
-			 setFire(bPower);
+			// find our enemy's location:
+			double ex = getX() + Math.sin(absBearing) * e.getDistance();
+			double ey = getY() + Math.cos(absBearing) * e.getDistance();
+	 
+			// Let's process the waves now:
+			for (int i=0; i < waves.size(); i++)
+			{
+				sparta currentWave = (sparta)waves.get(i);
+				if (currentWave.checkHit(ex, ey, getTime()))
+				{
+					waves.remove(currentWave);
+					i--;
+				}
+			}
+	 
 		
-			 
-			 
-			 //fires bPower
-			 fireBullet(bPower);
-			 // updates bullet power
-				buletpower = bPower;
-			 // updates targets energy
-				targetEnergy = e.getEnergy();
-		}
-	}
-	/**public void onScannedRobot(ScannedRobotEvent e) {
-		 
-		 while((++deltaTime) * (20.0 - 3.0 * bulletPower) < Point2D.Double.distance(myX, myY, predictedX, predictedY))
-			 {
-				 predictedX += Math.sin(targetHeading) * targetVelocity;
-				 predictedY += Math.cos(targetHeading) * targetVelocity;
-				 targetHeading += enemyHeadingChange;
-				 if(predictedX < 18.0 || predictedY < 18.0|| predictedX > fieldWidth - 18.0 || predictedY > fieldHeight - 18.0)
-				 {
-					 
-					 predictedX = Math.min(Math.max(18.0, predictedX), fieldWidth - 18.0);	
-					 predictedY = Math.min(Math.max(18.0, predictedY), fieldHeight - 18.0);
-					 break; 
-					 
-				 }
-				 double theta = Utils.normalAbsoluteAngle(Math.atan2(predictedX - getX(), predictedY - getY()));
-				 setTurnRadarRightRadians(Utils.normalRelativeAngle(
-						    absoluteBearing - getRadarHeadingRadians()));
-						setTurnGunRightRadians(Utils.normalRelativeAngle(
-						    theta - getGunHeadingRadians()));
-						
-			 }
-		 
-	
+			if (e.getVelocity() != 0)
+			{
+				if (Math.sin(e.getHeadingRadians()-absBearing)*e.getVelocity() < 0)
+					direction = -1;
+				else
+					direction = 1;
+			}
+			int[] currentStats = stats; // This seems silly, but I'm using it to
+						    // show something else later
+			sparta newWave = new sparta(getX(), getY(), absBearing, power,
+	                        direction, getTime(), currentStats);
+			int bestindex = 15;	
+			for (int i=0; i<31; i++)
+				if (currentStats[bestindex] < currentStats[i])
+					bestindex = i;
+	 
 			
-		 
-		 // creates a lock on a robot
-		 double radarTurn = getHeadingRadians() + e.getBearingRadians() - getRadarHeadingRadians();
-		 setTurnRadarRightRadians(Utils.normalRelativeAngle(radarTurn));
-		 
-		 
-		 //perdicts acording to power lvl
-		 double headOnBearing = getHeadingRadians() + e.getBearingRadians();
-		 double linearBearing = headOnBearing + Math.asin(e.getVelocity() / Rules.getBulletSpeed(bPower) * Math.sin(e.getHeadingRadians() - headOnBearing));
-		 setTurnGunRightRadians(Utils.normalRelativeAngle(linearBearing - getGunHeadingRadians()));
-		 setFire(bPower);
+			double guessfactor = (double)(bestindex - (stats.length - 1) / 2)
+	                        / ((stats.length - 1) / 2);
+			double angleOffset = direction * guessfactor * newWave.maxEscapeAngle();
+	                double gunAdjust = Utils.normalRelativeAngle(
+	                        absBearing - getGunHeadingRadians() + angleOffset);
+	                setTurnGunRightRadians(gunAdjust);
+	                
+	          
+            
+		
+		
+		
+	}
 	
-		}
-	}**/
 	
 	
 
